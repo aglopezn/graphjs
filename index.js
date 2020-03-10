01519ca0432a01c10f9a7b48496465e6d8849f82
Giovany Lopez <aglopezn@correo.udistrital.edu.co>
2020-03-10 14:21:25 -0500

    Se empieza la estructura de la pagina del metodo grafico.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 80
+const clientPath = __dirname + "/public/"
 
-app.use(express.static("./public"))
+app.use(express.static(clientPath))
 
 app.get("/", (req, res) => {
-    res.sendFile(__dir + "/public/index.html")
+    res.sendFile(clientPath + "index.html")
+})
+
+app.get("/metodo-grafico", (req, res) => {
+    res.sendFile(clientPath + "metodo-grafico.html")
 })
 
 app.listen(PORT, () => (console.log("running server on port", PORT)))
