001e6c8183cc95899c9531a0502911afa8dbea3c
Giovany Lopez <aglopezn@correo.udistrital.edu.co>
2020-03-16 11:22:07 -0500

    Resuelve por Gauss el sistemas de ecuaciones que forman las restricciones.

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 80
 const clientPath = __dirname + "/public/"
+//----------------------------------------
+const gauss = require("gaussian-elimination")
 
 app.use(express.static(clientPath))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.sendFile(clientPath + "index.html")
@@ -12,5 +16,30 @@ app.get("/", (req, res) => {
 app.get("/metodo-grafico", (req, res) => {
     res.sendFile(clientPath + "metodo-grafico.html")
 })
+
+app.post("/calcular-grafico", (req, res) => {
+    console.log(req.body)
+    let restricciones = organizarRestricciones(req.body)
+    let arr = [[0.12, 0.18, -0.17, 5.5],  
+           [0.06, 0.09, 0.15, -1.95],  
+           [0.22, -0.1, 0.06, 0.5]]
+    let sol = gauss(restricciones)
+    console.log(sol)
+    res.json(sol)
+})
+
+
+function organizarRestricciones(restriccionesJSON){
+    let cantidadRestricciones = restriccionesJSON.rx1.length
+    let restricciones = []
+    for (let i=0; i<cantidadRestricciones; i++){
+        let x = parseInt(restriccionesJSON.rx1[i])
+        let y = parseInt(restriccionesJSON.rx2[i])
+        let b = parseInt(restriccionesJSON.b[i])
+        let aux = [x,y,b]
+        restricciones.push(aux)
+    }
+    return restricciones
+}
 
 app.listen(PORT, () => (console.log("running server on port", PORT)))
